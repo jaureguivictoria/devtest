f1c05c57390f0956fc4f52b9fbd70497bf6d8603
Vicky Jauregui <jaureguivictoria@gmail.com>
2023-04-22 15:11:28 +0800

    Add fillable attributes to FoodCategory model

--- app/Models/FoodCategory.php
+++ app/Models/FoodCategory.php
@@ -6,10 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property integer $id 
+ * @property integer $id
  * @property string $name
 */
 class FoodCategory extends Model
 {
     use HasFactory;
+
+    protected $attributes = [
+        'name'
+    ];
 }
