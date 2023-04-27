24c23ccfef10344914f7b33469cf112faa9ca646
Vicky Jauregui <jaureguivictoria@gmail.com>
2023-04-27 16:09:58 +0800

    Update search placeholder text

--- resources/views/livewire/search-food-items.blade.php
+++ resources/views/livewire/search-food-items.blade.php
@@ -4,7 +4,7 @@
         <span class="absolute inset-y-0 left-0 flex items-center pl-2">
           <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><!-- ... --></svg>
         </span>
-        <input wire:model="search" class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search" type="text" name="search"/ id="search">
+        <input wire:model="search" class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search by name" type="text" name="search"/ id="search">
     </label>
 
     <table class="table-fixed mt-6">
