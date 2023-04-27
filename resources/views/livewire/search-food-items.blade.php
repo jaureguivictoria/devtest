28e853a4f9d94c8611532f0b82ba51b629becbf0
Vicky Jauregui <jaureguivictoria@gmail.com>
2023-04-27 14:38:18 +0800

    Updating HTML markup

--- resources/views/livewire/search-food-items.blade.php
+++ resources/views/livewire/search-food-items.blade.php
@@ -1,7 +1,13 @@
-<div>
-    <input type="text" wire:model="search" placeholder="Search food items..."/>
+<div class="not-prose relative bg-slate-50 rounded-xl overflow-hidden dark:bg-slate-800/25">
 
-    <table>
+    <label class="relative block">
+        <span class="absolute inset-y-0 left-0 flex items-center pl-2">
+          <svg class="h-5 w-5 fill-slate-300" viewBox="0 0 20 20"><!-- ... --></svg>
+        </span>
+        <input wire:model="search" class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search" type="text" name="search"/>
+    </label>
+
+    <table class="table-auto mt-6">
         <thead>
             <tr>
                 <th>Category</th>
